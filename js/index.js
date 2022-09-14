586664c99c988224e3e4e4f6692809f659e40d95
greewn <henry.greew@gmail.com>
2022-09-14 11:46:04 -0400

    io update and js

---
+++ js/index.js
@@ -0,0 +1,66 @@
+const command = require('./data/commands.json');
+
+function up_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function up_mUp(obj) {
+    obj.style.backgroundColor="#fff";
+}
+
+function down_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function down_mUp(obj) {
+    obj.style.backgroundColor="#fff";
+}
+
+function left_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function left_mUp(obj) {
+    obj.style.backgroundColor="#fff";
+}
+
+function right_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function right_mUp(obj) {
+    obj.style.backgroundColor="#fff";
+}
+
+function x_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function x_mUp(obj) {
+    obj.style.backgroundColor="#99B1C9";
+}
+
+function b_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function b_mUp(obj) {
+    obj.style.backgroundColor="#F2D027";
+}
+
+function y_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function y_mUp(obj) {
+    obj.style.backgroundColor="#B0D9BA";
+}
+
+function a_mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+}
+
+function a_mUp(obj) {
+    obj.style.backgroundColor="#D92E1E";
+}
+
